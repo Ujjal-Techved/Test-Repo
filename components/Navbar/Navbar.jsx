fc72981b1f584f75db2485a4f14354528fd9d24b
Ashish <ashishj@techved.com>
2025-01-22 11:16:46 +0530

    header WIP

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import styles from './Navbar.module.css'
+import { Button, Container } from 'reactstrap'
 
 const Navbar = () => {
   return (
-    <div>Navbar
-        <div className={styles.mainContainer}>hello world</div>
+    <div>
+      {/* Header for dekstop view */}
+      <Container className={styles.mainContainer}>
+        <div className={styles.main_header}>
+
+          <Button className= {styles.user_btn}>
+            <img src='../../images/header/toggler.svg' alt='Menu'></img>
+          </Button>
+         
+            <a href='/' className='logo'>
+              <img src='../../images/header/fgli-logo.svg' alt='FGLI'></img>
+            </a>
+     
+          <div className={styles.header_menu}>
+              <a>Term Insurance</a>
+              <a>Investment Plans</a>
+              <a>Learn</a>
+              <a>Support</a>
+          </div>
+          <div className={styles.header_userdetail}>
+                <a className={styles.commonlink_btn}>Pay Premium</a>
+                <Button className= {styles.user_btn}>
+                  <img src='../../images/header/user-icon.svg' alt='User'></img>
+                </Button>
+          </div>
+        </div>
+      </Container>
+
+      {/* Header for Mobile view */}
     </div>
   )
 }
