0b16140cafda0ecdcee3647d885803040b4df3f3
Karan Das <G.Karan.Das94@futuregenerali.in>
2025-01-28 12:18:15 +0530

    contact us

--- pages/contact-us/index.jsx
+++ pages/contact-us/index.jsx
@@ -4,11 +4,19 @@ import styles from './Contact.module.css';
 import Faqs from '@/components/Common/Faqs/Faqs';
 
 const ContactUs = () => {
+
+  const faqData = [
+    { question: 'What is React?', answer: 'React is a JavaScript library for building user interfaces.' },
+    { question: 'What is Reactstrap?', answer: 'Reactstrap is a library for using Bootstrap components in React.' },
+    { question: 'How does state work in React?', answer: 'State in React is an object that determines how a component renders and behaves.' },
+  ];
+
+
   return (
     <LandingLayout>
       Contact US
 
-      <Faqs/>
+      <Faqs faqItems={faqData} />
     </LandingLayout>
   )
 }
