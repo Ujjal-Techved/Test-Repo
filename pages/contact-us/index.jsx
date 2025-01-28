f463102a41417b78f84e76e4366b39b6970dadb3
Karan Das <G.Karan.Das94@futuregenerali.in>
2025-01-28 10:45:14 +0530

    contact us

---
+++ pages/contact-us/index.jsx
@@ -0,0 +1,13 @@
+import LandingLayout from '@/components/Layouts/LandingLayout';
+import React from 'react';
+import styles from './Contact.module.css';
+
+const ContactUs = () => {
+  return (
+    <LandingLayout>
+      Contact US
+    </LandingLayout>
+  )
+}
+
+export default ContactUs
