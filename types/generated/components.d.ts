b1aa775ce8c6868e87adea17ef92a3e7e6aa7e8d
Karan Das <G.Karan.Das94@futuregenerali.in>
2025-01-30 17:07:13 +0530

    contact us page added

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,39 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ContactUsContactCards extends Struct.ComponentSchema {
+  collectionName: 'components_contact_us_contact_cards';
+  info: {
+    displayName: 'ContactCards';
+  };
+  attributes: {
+    Description: Schema.Attribute.Text;
+    Description_Contact: Schema.Attribute.String;
+    Image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    LinkText: Schema.Attribute.String;
+    LinkUrl: Schema.Attribute.String;
+    Title: Schema.Attribute.String;
+  };
+}
+
+export interface ContactUsVisitUs extends Struct.ComponentSchema {
+  collectionName: 'components_contact_us_visituses';
+  info: {
+    displayName: 'VisitUs';
+  };
+  attributes: {
+    Description: Schema.Attribute.Text;
+    Image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    LinkText: Schema.Attribute.String;
+    LinkUrl: Schema.Attribute.String;
+    Title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'contact-us.contact-cards': ContactUsContactCards;
+      'contact-us.visit-us': ContactUsVisitUs;
+    }
+  }
+}
