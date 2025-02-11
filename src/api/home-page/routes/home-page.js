9e885b847dd353f92a4a3089e4c0c642b8078a36
Karan Das <G.Karan.Das94@futuregenerali.in>
2025-02-11 10:37:27 +0530

    homepage template added

--- src/api/home-page/routes/home-page.js
+++ src/api/home-page/routes/home-page.js
@@ -6,4 +6,23 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::home-page.home-page');
+module.exports = createCoreRouter('api::home-page.home-page',{
+    config: {
+        find: {
+            middlewares: [
+                {
+                    name: "api::home-page.home-page-default-populate",
+                    config: { type: "find" },
+                },
+            ],
+        },
+        findOne: {
+            middlewares: [
+                {
+                    name: "api::home-page.home-page-default-populate",
+                    config: { type: "findOne" },
+                },
+            ],
+        },
+    },
+});
