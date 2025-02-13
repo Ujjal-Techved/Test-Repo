527a78e8c49efc5a93daefc9c44741dd70e1e318
Karan Das <G.Karan.Das94@futuregenerali.in>
2025-02-13 10:15:58 +0530

    about us page template

--- src/api/about-us/routes/about-us.js
+++ src/api/about-us/routes/about-us.js
@@ -6,4 +6,23 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::about-us.about-us');
+module.exports = createCoreRouter('api::about-us.about-us', {
+    config: {
+        find: {
+            middlewares: [
+                {
+                    name: "api::about-us.about-us-default-populate",
+                    config: { type: "find" },
+                },
+            ],
+        },
+        findOne: {
+            middlewares: [ 
+                {
+                    name: "api::about-us.about-us-default-populate",
+                    config: { type: "findOne" },
+                },
+            ],
+        },
+    },
+});
