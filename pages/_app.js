9c01c22c9c866ddfec6757fa6afdb05d0165908f
Nikunjm <G.Nikunj.Makwana93@futuregenerali.in>
2025-02-19 16:12:40 +0530

    GA4 added

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,25 @@ import '../styles/globals.css';
 import '../styles/styles.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
+const GA_MEASUREMENT_ID = "3124600306"; // Replace with your GA4 Measurement ID
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Initialize GA4 config
+      window.gtag("config", GA_MEASUREMENT_ID);
+    }
+  }, []);
+
+  return (
+    <>
+      {/* Automatically track page views */}
+      <GoogleAnalytics trackPageViews gaMeasurementId={GA_MEASUREMENT_ID} />
+      <Component {...pageProps} />;
+    </>
+  )
 }
