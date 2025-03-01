b525cd8a561eecbde0293a6c9e5455895ae74856
Karan Das <G.Karan.Das94@futuregenerali.in>
2025-03-01 12:41:23 +0530

    plp page

---
+++ pages/product-landing/index.jsx
@@ -0,0 +1,12 @@
+import LandingLayout from '@/components/Layouts/LandingLayout'
+import React from 'react'
+
+const index = () => {
+  return (
+    <LandingLayout>
+        
+    </LandingLayout>
+  )
+}
+
+export default index
