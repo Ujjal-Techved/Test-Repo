2132d08f10efa8e90821c408932d6317277db1bf
Ashish <ashishj@techved.com>
2025-04-22 18:36:16 +0530

    Home page changes new sections v2

--- components/ContactUs/InvestPlan/InvestPlan.jsx
+++ components/ContactUs/InvestPlan/InvestPlan.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import styles from './InvestPlan.module.css';
 import { Container } from 'reactstrap';
 import { useRouter } from "next/navigation";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 const InvestPlan = ({ investPlanData }) => {
     const router = useRouter();
@@ -15,15 +17,22 @@ const InvestPlan = ({ investPlanData }) => {
     }
 
     // Destructure API response data for cleaner code
-    const { 
-        Title, 
-        Description, 
-        PlayStore_Link, 
-        AppStore_Link, 
-        QR_Code_Image, 
-        PlayStore_Image, 
-        AppStore_Image 
+    const {
+        Title,
+        Description,
+        PlayStore_Link,
+        AppStore_Link,
+        QR_Code_Image,
+        PlayStore_Image,
+        AppStore_Image
     } = investPlanData?.AppLink;
+
+    const validationSchema = Yup.object().shape({
+        phone: Yup.string()
+            .min(10, "Enter a valid 10-digit mobile no.")
+            .max(10, "Enter a valid 10-digit mobile no.")
+            .required("Phone number is required"),
+    });
 
     return (
         <Container>
@@ -37,7 +46,7 @@ const InvestPlan = ({ investPlanData }) => {
                         {/* Form section for entering a mobile number */}
                         <div className={styles.form_applink}>
                             <label>{Description}</label>
-                            <div className={styles.applink_main}>
+                            {/* <div className={styles.applink_main}>
                                 <input
                                     type="tel"
                                     readOnly={false} // Allows user input
@@ -55,23 +64,61 @@ const InvestPlan = ({ investPlanData }) => {
                                     maxLength="10" // Restricts input to 10 characters
                                     pattern="\d{10}" // Enforces a 10-digit format
                                 />
-                                {/* Button to send app link via Play Store */}
+         
                                 <button
                                     onClick={() => router.push(PlayStore_Link)}
                                     className="whiteBtn"
                                 >
                                     Send App Link
                                 </button>
-                            </div>
+                            </div> */}
+
+                            <Formik
+                                initialValues={{
+                                    name: "",
+                                    phone: ""
+                                }}
+                                validateOnBlur={false}
+                                validateOnChange={false}
+                                validationSchema={validationSchema}
+                                onSubmit={(values) => {
+                                    console.log("Form submitted:", values);
+                                }}
+                            >
+                                {({ errors, setFieldValue, setFieldError }) => (
+                                    <Form className={styles.leadForm}>
+                                       
+                                        <div className={`${styles.formGroup} ${errors.phone ? styles.error : ""} `}>
+                                            <label htmlFor="phone">Mobile Number *</label>
+                                            <Field
+                                                type="tel"
+                                                name="phone"
+                                                placeholder='Enter your Mobile Number'
+                                                className={styles.inputField}
+                                                maxLength={10}
+                                                onChange={(e) => {
+                                                    setFieldValue('phone', e.target.value.replace(/\D/g, ""));
+                                                    setFieldError('phone', '');
+                                                }}
+                                            />
+                                            <p className={styles.errorMsg}>{errors.phone}</p>
+                                        </div>
+
+                                        <button type='submit' className="whiteBtn px-4">Send App link</button>
+                                    </Form>
+                                )}
+                            </Formik>
                         </div>
+
+
 
                         {/* Section for displaying QR code and download links */}
                         <div className={styles.scanner_links}>
                             {/* QR Code Image for scanning */}
                             <div>
-                                <img 
-                                    src={process.env.NEXT_PUBLIC_APP_API + QR_Code_Image?.url} 
-                                    alt={QR_Code_Image?.alternativeText} 
+                                <img
+                                    src={process.env.NEXT_PUBLIC_APP_API + QR_Code_Image?.url}
+                                    alt={QR_Code_Image?.alternativeText}
                                 />
                             </div>
 
@@ -79,16 +126,16 @@ const InvestPlan = ({ investPlanData }) => {
                             <div className={styles.links_wrapper}>
                                 {/* Play Store link */}
                                 <a href={PlayStore_Link} className={styles.links_btn}>
-                                    <img 
-                                        src={process.env.NEXT_PUBLIC_APP_API + PlayStore_Image?.url} 
-                                        alt={PlayStore_Image?.alternativeText} 
+                                    <img
+                                        src={process.env.NEXT_PUBLIC_APP_API + PlayStore_Image?.url}
+                                        alt={PlayStore_Image?.alternativeText}
                                     />
                                 </a>
                                 {/* App Store link */}
                                 <a href={AppStore_Link} className={styles.links_btn}>
-                                    <img 
-                                        src={process.env.NEXT_PUBLIC_APP_API + AppStore_Image?.url} 
-                                        alt={AppStore_Image?.alternativeText} 
+                                    <img
+                                        src={process.env.NEXT_PUBLIC_APP_API + AppStore_Image?.url}
+                                        alt={AppStore_Image?.alternativeText}
                                     />
                                 </a>
                             </div>
