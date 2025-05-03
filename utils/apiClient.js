8a1afca4438ac42f3b028904ae163939569cc7ff
nikunj <nikunjm@techved.com>
2025-05-03 19:39:21 +0530

    code added for PLP page binding for first 3 section

--- utils/apiClient.js
+++ utils/apiClient.js
@@ -5,6 +5,7 @@
  * @param {object} options - Additional options for the fetch request (e.g., method, headers)
  * @returns {Promise<object>} - Returns JSON response from the API
  */
+
 export const apiClient = async (endpoint, options = {}) => {
     // ✅ Get the Base URL from Environment Variables (Must start with NEXT_PUBLIC_ for client-side use)
     const baseUrl = process.env.NEXT_PUBLIC_APP_API;
@@ -29,5 +30,39 @@ export const apiClient = async (endpoint, options = {}) => {
     } catch (error) {
         console.error("API Fetch Error:", error);
         throw error; // Re-throw the error to be handled where the function is called
+    }
+};
+
+
+ /**
+ * Lead Form Submission API Call
+ * 
+ * @param {string} endpoint - The API endpoint (e.g., "/submit-lead")
+ * @param {object} payload - The body data to send
+ * @returns {Promise<void>}
+ */
+export const leadFormAPICall = async (payload) => {
+    const baseUrl = process.env.NEXT_PUBLIC_LEAD_FORM_API_URL;
+
+    try {
+        const response = await fetch(baseUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "partnertoken": process.env.NEXT_PUBLIC_LEAD_FORM_API_TOKEN,
+            },
+            body: JSON.stringify(payload),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Lead Form API Error: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log("API Success:", data);
+        alert("Lead submitted successfully!");
+    } catch (error) {
+        console.error("Submission error:", error);
+        alert("Something went wrong. Please try again.");
     }
 };
